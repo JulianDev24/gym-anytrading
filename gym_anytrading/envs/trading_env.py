c5b4f4203f38bb985a9bf2e68dceef64704f2e09
JulianDev <comegamer64@gmail.com>
2022-05-06 07:16:09 +0200

    Update Methods

--- gym_anytrading/envs/trading_env.py
+++ gym_anytrading/envs/trading_env.py
@@ -1,9 +1,10 @@
+from enum import Enum
+
 import gym
+import matplotlib.pyplot as plt
+import numpy as np
 from gym import spaces
 from gym.utils import seeding
-import numpy as np
-from enum import Enum
-import matplotlib.pyplot as plt
 
 
 class Actions(Enum):
@@ -20,7 +21,6 @@ class Positions(Enum):
 
 
 class TradingEnv(gym.Env):
-
     metadata = {'render.modes': ['human']}
 
     def __init__(self, df, window_size):
@@ -29,7 +29,9 @@ class TradingEnv(gym.Env):
         self.seed()
         self.df = df
         self.window_size = window_size
-        self.prices, self.signal_features = self._process_data()
+
+        self.prices, self.time_series, self.signal_features = self._process_data()
+
         self.shape = (window_size, self.signal_features.shape[1])
 
         # spaces
@@ -49,11 +51,9 @@ class TradingEnv(gym.Env):
         self._first_rendering = None
         self.history = None
 
-
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
-
 
     def reset(self):
         self._done = False
@@ -66,7 +66,6 @@ class TradingEnv(gym.Env):
         self._first_rendering = True
         self.history = {}
         return self._get_observation()
-
 
     def step(self, action):
         self._done = False
@@ -82,7 +81,7 @@ class TradingEnv(gym.Env):
 
         trade = False
         if ((action == Actions.Buy.value and self._position == Positions.Short) or
-            (action == Actions.Sell.value and self._position == Positions.Long)):
+                (action == Actions.Sell.value and self._position == Positions.Long)):
             trade = True
 
         if trade:
@@ -91,19 +90,39 @@ class TradingEnv(gym.Env):
 
         self._position_history.append(self._position)
         observation = self._get_observation()
+
+        window_ticks = np.arange(len(self._position_history))
+
+        short_ticks = []
+        long_ticks = []
+
+        for i, tick in enumerate(window_ticks):
+            if self._position_history[i] == Positions.Short:
+                short_ticks.append(tick)
+            elif self._position_history[i] == Positions.Long:
+                long_ticks.append(tick)
+
+        window_ticks = np.arange(len(self._position_history))
+
+        ticks = []
+        for i, tick in enumerate(window_ticks):
+            ticks.append(tick)
+
         info = dict(
-            total_reward = self._total_reward,
-            total_profit = self._total_profit,
-            position = self._position.value
+            total_reward=self._total_reward,
+            total_profit=self._total_profit,
+            position=self._position.value,
+            short_ticks=short_ticks,
+            long_ticks=long_ticks,
+            short_long_ticks_combined=self.prices[ticks]
         )
+
         self._update_history(info)
 
         return observation, step_reward, self._done, info
 
-
     def _get_observation(self):
-        return self.signal_features[(self._current_tick-self.window_size):self._current_tick]
-
+        return self.signal_features[(self._current_tick - self.window_size):self._current_tick]
 
     def _update_history(self, info):
         if not self.history:
@@ -111,7 +130,6 @@ class TradingEnv(gym.Env):
 
         for key, value in info.items():
             self.history[key].append(value)
-
 
     def render(self, mode='human'):
 
@@ -140,51 +158,70 @@ class TradingEnv(gym.Env):
 
         plt.pause(0.01)
 
-
     def render_all(self, mode='human'):
         window_ticks = np.arange(len(self._position_history))
-        plt.plot(self.prices)
+        plt.plot(self.time_series, self.prices)
 
         short_ticks = []
         long_ticks = []
+
         for i, tick in enumerate(window_ticks):
             if self._position_history[i] == Positions.Short:
                 short_ticks.append(tick)
             elif self._position_history[i] == Positions.Long:
                 long_ticks.append(tick)
 
-        plt.plot(short_ticks, self.prices[short_ticks], 'ro')
-        plt.plot(long_ticks, self.prices[long_ticks], 'go')
+        plt.plot(self.time_series[short_ticks], self.prices[short_ticks], 'ro')
+        plt.plot(self.time_series[long_ticks], self.prices[long_ticks], 'go')
 
         plt.suptitle(
             "Total Reward: %.6f" % self._total_reward + ' ~ ' +
             "Total Profit: %.6f" % self._total_profit
         )
-        
-        
+
+    def short_long_ticks_combined(self):
+        window_ticks = np.arange(len(self._position_history))
+
+        ticks = []
+        for i, tick in enumerate(window_ticks):
+            ticks.append(tick)
+
+        return self.prices[ticks]
+
+    def short_long_ticks(self):
+        window_ticks = np.arange(len(self._position_history))
+
+        short_ticks = []
+        long_ticks = []
+
+        for i, tick in enumerate(window_ticks):
+            if self._position_history[i] == Positions.Short:
+                short_ticks.append(tick)
+            elif self._position_history[i] == Positions.Long:
+                long_ticks.append(tick)
+
+        return short_ticks, long_ticks
+
+    def price_time_series(self):
+        return self.prices, self.time_series
+
     def close(self):
         plt.close()
-
 
     def save_rendering(self, filepath):
         plt.savefig(filepath)
 
-
     def pause_rendering(self):
         plt.show()
-
 
     def _process_data(self):
         raise NotImplementedError
 
-
     def _calculate_reward(self, action):
         raise NotImplementedError
 
-
     def _update_profit(self, action):
         raise NotImplementedError
-
 
     def max_possible_profit(self):  # trade fees are ignored
         raise NotImplementedError
